860a3ab6eb1cf46a9996442afd48ee0c876568f9
chongaaaa <hengpeilin@gmail.com>
2024-07-29 00:37:15 +0800

    omO

---
+++ home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# emoji icon link: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Mobie", page_icon=":movie_camera:", layout= "wide")
+
+st.title("Welcome to MOBIE")
