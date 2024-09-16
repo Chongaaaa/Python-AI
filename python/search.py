ae3021168860d89f06f201469cdaae616227bac4
chongaaaa <hengpeilin@gmail.com>
2024-09-16 23:10:24 +0800

    update notebook

--- python/search.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-# Load the dataset
-df = pd.read_csv(r'C:/Users/Asus/Desktop/Python/dataset/imdb_top_1000.csv')
-
-# Optionally, print the DataFrame
-print(df.head())
